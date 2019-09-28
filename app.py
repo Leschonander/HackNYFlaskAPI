ba27d544e414c3917bb3646e8ca0b9cb8d417b10
Leschonander <33600597+Leschonander@users.noreply.github.com>
2019-09-28 18:00:33 -0400

    Cors?

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
