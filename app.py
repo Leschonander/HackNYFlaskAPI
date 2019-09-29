2c71db64402311f05135549216dfdab0750a0dbc
Leschonander <33600597+Leschonander@users.noreply.github.com>
2019-09-28 23:11:06 -0400

    Cleaned data
    Refining the api...

--- app.py
+++ app.py
@@ -10,28 +10,28 @@ from sklearn import preprocessing
 import pandas as pd
 
 
-data = pd.read_csv("Cleaned_ASR_Data.csv")
-
-X = data[['Years_of_Schooling', 'English_Skill_Now']]
-y = data['Income_Per_Hour']
-
-income_regression = LinearRegression().fit(X, y)
-
-
+# {"Schooling": 16, "English": 0, "Work Hours"}   
 @app.route('/')
 def hello_world():
    return 'Hello, World!'
 
 @app.route("/predict", methods=["POST"])
 def predict():
+   data = pd.read_csv("Cleaned_ASR_Data.csv")
 
-    json = request.json
+   X = data[['Years_of_Schooling', 'English_Skill_Now']]
+   y = data['Income_Per_Hour']
 
-    result = income_regression.predict([[
-        float(json["Schooling"]),
-        float(json["English"])
-    ]])
-    
-    result = {"Result": float(result[0])}
+   income_regression = LinearRegression().fit(X, y)
 
-    return jsonify(result)
+   json = request.json
+
+   result = income_regression.predict([[
+      float(json["Schooling"]),
+      float(json["English"])
+   ]])
+
+   result = {"Result": float(result[0])}
+
+   return jsonify(result)
+
